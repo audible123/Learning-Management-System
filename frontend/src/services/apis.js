4bc117f5ecd8143a91a7464d1fead2506a245484
ADITYA SINGH <121515934+audible123@users.noreply.github.com>
2024-06-25 09:22:50 +0530

    fixing bugs

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
+const BASE_URL = "https://learninghub-apis.vercel.app/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
